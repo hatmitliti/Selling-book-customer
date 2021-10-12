e5cad5195ff0d2d86cefad0b4f90127b34476485
Nguyễn Trường <truongth2904@gmail.com>
2021-10-12 10:58:42 +0700

    update layout

--- app/src/main/java/com/example/customer/MainActivity.java
+++ app/src/main/java/com/example/customer/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+        setContentView(R.layout.xac_thuc_dang_ky);
     }
 }
