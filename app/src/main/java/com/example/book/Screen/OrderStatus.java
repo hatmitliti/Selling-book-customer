e5ec6a03c800af8264081e71519d13db32e3471d
lenam <leho.tdc2019@gmail.com>
2021-11-06 16:30:58 +0700

    update layout

--- app/src/main/java/com/example/book/Screen/OrderStatus.java
+++ app/src/main/java/com/example/book/Screen/OrderStatus.java
@@ -5,13 +5,39 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.book.R;
 
 public class OrderStatus extends AppCompatActivity {
+    Toolbar tb;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.trang_thai_don_hang);
+        setControl();
+        setEvent();
+        toolbar();
+    }
+
+    private void toolbar() {
+        setSupportActionBar(tb);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
+        tb.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+    }
+
+    private void setEvent() {
+    }
+
+    private void setControl() {
+        tb = findViewById(R.id.tb);
     }
 }
