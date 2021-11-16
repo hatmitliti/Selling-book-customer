9f6fbab2be773abc0965f43bd6610c31a9becf6c
Trường Vũ <truongvu6021@gmail.com>
2021-11-16 16:32:51 +0700

    UI update for sign in screen

--- app/src/main/java/com/example/book/Screen/Login.java
+++ app/src/main/java/com/example/book/Screen/Login.java
@@ -20,8 +20,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 
 public class Login extends AppCompatActivity {
@@ -38,7 +36,7 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.dang_nhap);
         setControl();
         auth = FirebaseAuth.getInstance();
         remember();
